25a63a105e89caaafc9f84a48b9da26cb3ea567a
Daniel Alejandro Chilito Giron <chilito45giron@gmail.com>
2026-01-05 21:36:33 -0500

    Traduccion de la pagina al español y el nuevo camopo de coreo electronico

--- blood_ink/settings.py
+++ blood_ink/settings.py
@@ -132,13 +132,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es"
 
 TIME_ZONE = "America/Bogota"
 
 USE_I18N = True
 
 USE_TZ = True
+USE_L10N = True
 
 
 # Static files (CSS, JavaScript, Images)
--- login/forms.py
+++ login/forms.py
@@ -7,6 +7,9 @@ class SignIn(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'input'
     }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'input'
+    }))
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'input'
@@ -17,6 +20,10 @@ class SingUp (UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'input'
     }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'input'
+    }))
+
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'input'
     }))
@@ -24,7 +31,20 @@ class SingUp (UserCreationForm):
         'class': 'input'
     }))
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+            return user
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este email ya esstá registrado.")
+        return email
+
     class Meta:
         model = User
         # Definimos los campos que se mostrarán
-        fields = ("username",)
+        fields = ("username", "email",)
