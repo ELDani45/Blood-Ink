203a6ded39fb2782c43d4d74815feb9fbd2a7ce8
Daniel Alejandro Chilito Giron <chilito45giron@gmail.com>
2026-02-05 18:04:14 -0500

    card de perfil hecho, tambien expandimos el modelo de user y añadi signals al momento de crear un usuario: NOTA: como los usuarios antes de este commit no tiene el modelo de profile en su usuario al momento de iniciar sesion con un usuario sin antes de este commit, se producerá un error

--- login/forms.py
+++ login/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from account.models import Profile
 
 
 class SignIn(AuthenticationForm):
