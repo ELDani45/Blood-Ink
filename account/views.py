a32f39fae63461a81849ead2a2b87177e2afe8c1
Daniel Alejandro Chilito Giron <chilito45giron@gmail.com>
2026-02-06 23:05:33 -0500

    desarrollando el template de perfil, aun sin twerminar, faltan bastantes cositas (hay algo en el diseño que no me gusta)
    Signed-off-by: Daniel Alejandro Chilito Giron <chilito45giron@gmail.com>

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic import DetailView
 from django.contrib.auth.models import User
+from novels.models import Description
 # Create your views here.
 
 
@@ -11,3 +12,8 @@ class Profie(DetailView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['description'] = Description.objects.filter(author=self.object)
+        return context
